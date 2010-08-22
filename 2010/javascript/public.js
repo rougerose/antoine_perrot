99089e2cda3d823f0b0ce2079e50aae7311fb270
christophe le drean <christophe@rouge-rose.net>
2010-08-22 18:59:13 +0200

    Firefox 3 ne gère pas les transitions CSS 3.

--- 2010/javascript/public.js
+++ 2010/javascript/public.js
@@ -4,4 +4,52 @@ $(document).ready(function(){
 	$("#grille").click(function(){
 		$("body").toggleClass("grille");
 	});
+	
+	// transitions CSS3 non disponibles dans Firefox 3 (max)
+	if (typeof getStyleProperty('transition') == 'undefined') {
+		// property is supported
+		$("#nav li a").addClass("ff3").hover(
+			function() {
+				$(this).stop().animate({paddingLeft:"30px"},600);
+			},
+			function() {
+				$(this).stop().animate({paddingLeft:"40px"},600);
+			}
+		);
+	}
 });
+
+
+/**
+ * @method getStyleProperty
+ * @param {String} propName style property to test
+ * @param {HTMLElement} element optional optional element to test
+ * @return {String | undefined}
+ *
+ * @example getStyleProperty('borderRadius');
+ * http://perfectionkills.com/feature-testing-css-properties/
+ */
+var getStyleProperty = (function(){
+
+  var prefixes = ['Moz', 'Webkit', 'Khtml', 'O', 'Ms'];
+
+  function getStyleProperty(propName, element) {
+    element = element || document.documentElement;
+    var style = element.style,
+        prefixed;
+
+    // test standard property first
+    if (typeof style[propName] == 'string') return propName;
+
+    // capitalize
+    propName = propName.charAt(0).toUpperCase() + propName.slice(1);
+
+    // test vendor specific properties
+    for (var i=0, l=prefixes.length; i<l; i++) {
+      prefixed = prefixes[i] + propName;
+      if (typeof style[prefixed] == 'string') return prefixed;
+    }
+  }
+
+  return getStyleProperty;
+})();
