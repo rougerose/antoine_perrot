9fbfeb234bf1d3d5bedf897470fa61fe623b34e5
christophe le drean <christophe@rouge-rose.net>
2010-10-13 22:26:01 +0200

    suite du squelettes article

--- 2010/lang/local_fr.php
+++ 2010/lang/local_fr.php
@@ -1,7 +1,19 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// I
+	'informations' => 'informations',
+	
 	// L
-	'lire_la_suite' => 'lire la suite'
+	'lire_la_suite' => 'lire la suite',
+	
+	// 0
+	'ou' => 'o&ugrave;&nbsp;?',
+	
+	// P
+	'publie' => 'publi&eacute; le',
+	
+	// Q
+	'quand' => 'quand&nbsp;?'
 );
 
 ?>
