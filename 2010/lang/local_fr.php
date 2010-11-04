502527f14411e6e7a66286d4a3d276250000a53e
christophe le drean <christophe@rouge-rose.net>
2010-11-04 18:14:46 +0100

    suite (et notamment) : - ajustement des styles - pagination - recherche : ajout des rubriques, des documents et des images

--- 2010/lang/local_fr.php
+++ 2010/lang/local_fr.php
@@ -2,26 +2,38 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	// A
+	'afficher_document' => 'afficher le document',
+	'afficher_image' => 'afficher l&#x27;image',
 	'afficher_site' => 'afficher le site',
+	'article' => 'article',
+	'articles' => 'articles',
 	'article_suivant' => 'article suivant',
 	
 	// D
 	'date' => 'date',
+	'document' => 'document',
+	'documents' => 'documents',
 	'document_sans_titre' => 'document sans titre',
 	
 	// F
 	'fichier' => 'fichier',
 	
 	// I
+	'image' => 'image',
+	'images' => 'images',
 	'informations' => 'informations',
 	
 	// L
 	'lire_la_suite' => 'lire la suite',
 	
+	// M
+	'mise_a_jour' => 'mise &agrave; jour',
+	
 	// 0
 	'ou' => 'o&ugrave;&nbsp;?',
 	
 	// P
+	'pages' => 'pages',
 	'publie' => 'publi&eacute; le',
 	'ps' => 'Post-scriptum',
 	
@@ -29,7 +41,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'quand' => 'quand&nbsp;?',
 	
 	// R
+	'resultats_recherche' => 'r&eacute;sultats de la recherche',
 	'retour_rubrique' => 'retour &agrave; la rubrique',
+	'rubrique' => 'rubrique',
+	'rubriques' => 'rubriques',
 	
 	// T
 	'titre' => 'titre',
